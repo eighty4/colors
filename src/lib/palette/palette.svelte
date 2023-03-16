a82f87d983e2fc013743e0dd62fb252590264e05
Adam McKee <adam.be.g84d@gmail.com>
2023-03-16 16:49:19 -0500

    initial commit of sveltekit gen

---
+++ src/lib/palette/palette.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Swatch from './swatch.svelte'
+</script>
+
+<div>
+    <ol>
+        <li>
+            <Swatch/>
+        </li>
+        <li>
+            <Swatch/>
+        </li>
+    </ol>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Palette from '$lib/palette/palette.svelte'
+</script>
+
+<h1>Colors!</h1>
+
+<Palette/>
+<Palette/>
+<Palette/>
