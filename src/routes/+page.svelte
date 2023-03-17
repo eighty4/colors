521c31c4103dc727fc6728545a3eb79ecfac231d
Adam McKee <adam.be.g84d@gmail.com>
2023-03-17 15:22:54 -0500

    nest web app under /web

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Palette from '$lib/palette/palette.svelte'
-</script>
-
-<h1>Colors!</h1>
-
-<Palette/>
-<Palette/>
-<Palette/>
---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import Palette from '$lib/palette/palette.svelte'
+</script>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    header {
+        background: purple;
+        width: 100%;
+        height: 50px;
+    }
+    div {
+        display: flex;
+    }
+    aside {
+        background: red;
+        width: 50px;
+        height: 100%;
+    }
+    main {
+        background: blue;
+        flex: 1;
+        height: 100%;
+    }
+</style>
+
+<header>
+    <h1>eighty4 / Colors</h1>
+</header>
+<div>
+    <aside>
+        <p>wtf</p>
+    </aside>
+    <main>
+        <p>wtf</p>
+    </main>
+</div>
+
+<Palette/>
+<Palette/>
+<Palette/>
