501466468f4066141196304fb8c6c8c3045722a5
Adam McKee <adam.be.g84d@gmail.com>
2023-04-04 15:49:50 -0500

    refactor page components out of colors.svelte

--- web/src/lib/colors.svelte
+++ web/src/lib/colors.svelte
@@ -1,6 +1,8 @@
 <script>
-    import ScrapeUrlForm from '$lib/url-form.svelte'
-    import Palette from '$lib/palette/palette.svelte'
+    import Header from './header.svelte'
+    import Menu from './menu.svelte'
+    import Palette from './palette/palette.svelte'
+    import ScrapeUrlForm from './url-form.svelte'
 
     let palettes = null
 
@@ -17,22 +19,7 @@
     }
 </script>
 
-<header>
-    <div id="app-title">
-        <a id="eighty4-link" href="https://eighty4.tech">eighty4<span id="tld">.tech</span></a>
-        <span id="slash">/</span>
-        <span id="app-link">colors</span>
-    </div>
-    <div id="spacer"></div>
-    <a id="github-link" href="https://github.com/eighty4/colors">
-        <img alt="GitHub logo" src="github-mark-white.svg"/>
-    </a>
-</header>
-<aside>
-    <button id="create-palette-link" on:click={addPalette}>
-        <span>Add Palette</span>
-    </button>
-</aside>
+<Header/>
 <main>
     <div class="content-spacing">
         <ScrapeUrlForm/>
@@ -45,6 +32,7 @@
         {/each}
     {/if}
 </main>
+<Menu on:add-palette={addPalette}/>
 
 <style>
     :root {
@@ -62,80 +50,13 @@
         --border-hover-color: #aaa;
     }
 
-    header {
-        background: var(--background-header);
-        border-bottom: var(--border-size) solid var(--border-color);
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        height: calc(var(--header-height) - var(--border-size));
-        display: flex;
-        flex-direction: row;
-        justify-content: start;
-        align-items: center;
-        padding-left: calc(1.5 * var(--aside-width));
-        padding-right: var(--half-aside-width);
-    }
-
-    header #app-title {
-        font-size: 1.5rem;
-        letter-spacing: .075rem;
-        font-family: Quicksand, sans-serif;
-        color: var(--text-primary-color);
-        user-select: none;
-    }
-
-    header #app-title #eighty4-link {
-        text-decoration: none;
-        text-underline-offset: .175rem;
-        color: var(--text-primary-color);
-    }
-
-    header #app-title #eighty4-link:hover {
-        text-decoration: underline;
-        text-decoration-color: mediumvioletred;
-    }
-
-    header #app-title #eighty4-link #tld {
-        font-size: 1rem;
-    }
-
-    header #app-title #slash {
-        margin-left: .5rem;
-    }
-
-    header #app-title #app-link {
-        border: var(--border-size) solid transparent;
-        cursor: pointer;
-        transition: border 300ms linear;
-        margin-left: .1rem;
-        padding: .2rem .4rem;
-        border-radius: .1rem;
-    }
-
-    header:hover #app-title #app-link {
-        border: var(--border-size) solid var(--border-lite-highlight-color);
-    }
-
-    header #app-title #app-link:hover {
-        border: var(--border-size) solid var(--border-hover-color);
-    }
-
-    header #spacer {
-        flex: 1;
-    }
-
-    header #github-link, header #github-link img {
-        height: calc(.5 * var(--header-height));
-    }
-
     main {
         background: var(--background-main);
         color: var(--text-primary-color);
         padding: var(--half-aside-width);
+        padding-left: calc(1.5 * var(--aside-width));
         position: fixed;
-        left: var(--aside-width);
+        left: 0;
         right: 0;
         bottom: 0;
         top: var(--header-height);
@@ -147,51 +68,5 @@
 
     .content-spacing {
         margin-bottom: var(--half-aside-width);
-    }
-
-    aside {
-        background: var(--background-aside);
-        position: fixed;
-        left: 0;
-        bottom: 0;
-        top: var(--header-height);
-        width: calc(var(--aside-width) - var(--border-size));
-        height: 100%;
-        display: flex;
-        align-content: center;
-        justify-content: center;
-        border-right: var(--border-size) solid var(--border-color);
-    }
-
-    aside #create-palette-link {
-        background-color: transparent;
-        margin-top: var(--half-aside-width);
-        align-self: baseline;
-        user-select: none;
-        cursor: pointer;
-        border: var(--border-size) solid transparent;
-        border-radius: .1rem;
-        padding-bottom: .6rem;
-        transition: border 300ms linear;
-    }
-
-    aside #create-palette-link span {
-        color: var(--text-primary-color);
-        font-family: BarlowThin, sans-serif;
-        writing-mode: vertical-lr;
-        text-orientation: upright;
-        letter-spacing: -.32rem;
-        text-transform: uppercase;
-        font-size: 1.9rem;
-        font-weight: bold;
-        word-spacing: -.66rem;
-    }
-
-    aside:hover #create-palette-link {
-        border: var(--border-size) solid var(--border-medium-highlight-color);
-    }
-
-    aside:hover #create-palette-link:hover {
-        border: var(--border-size) solid var(--border-hover-color);
     }
 </style>
---
+++ web/src/lib/header.svelte
@@ -0,0 +1,81 @@
+<header>
+    <div id="app-title">
+        <a id="eighty4-link" href="https://eighty4.tech">eighty4<span id="tld">.tech</span></a>
+        <span id="slash">/</span>
+        <span id="app-link">colors</span>
+    </div>
+    <div id="spacer"></div>
+    <a id="github-link" href="https://github.com/eighty4/colors">
+        <img alt="GitHub logo" src="github-mark-white.svg"/>
+    </a>
+</header>
+
+<style>
+    header {
+        background: var(--background-header);
+        border-bottom: var(--border-size) solid var(--border-color);
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        height: calc(var(--header-height) - var(--border-size));
+        display: flex;
+        flex-direction: row;
+        justify-content: start;
+        align-items: center;
+        padding-left: calc(1.5 * var(--aside-width));
+        padding-right: var(--half-aside-width);
+    }
+
+    header #app-title {
+        font-size: 1.5rem;
+        letter-spacing: .075rem;
+        font-family: Quicksand, sans-serif;
+        color: var(--text-primary-color);
+        user-select: none;
+    }
+
+    header #app-title #eighty4-link {
+        text-decoration: none;
+        text-underline-offset: .175rem;
+        color: var(--text-primary-color);
+    }
+
+    header #app-title #eighty4-link:hover {
+        text-decoration: underline;
+        text-decoration-color: mediumvioletred;
+    }
+
+    header #app-title #eighty4-link #tld {
+        font-size: 1rem;
+    }
+
+    header #app-title #slash {
+        margin-left: .5rem;
+    }
+
+    header #app-title #app-link {
+        border: var(--border-size) solid transparent;
+        cursor: pointer;
+        transition: border 300ms linear;
+        margin-left: .1rem;
+        padding: .2rem .4rem;
+        border-radius: .1rem;
+    }
+
+    header:hover #app-title #app-link {
+        border: var(--border-size) solid var(--border-lite-highlight-color);
+    }
+
+    header #app-title #app-link:hover {
+        border: var(--border-size) solid var(--border-hover-color);
+    }
+
+    header #spacer {
+        flex: 1;
+    }
+
+    header #github-link, header #github-link img {
+        height: calc(.5 * var(--header-height));
+    }
+</style>
